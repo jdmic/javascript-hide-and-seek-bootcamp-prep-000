011cf06d8bd62ed6c298fa776eb8896e5dbfeced
Jimmy Michalec <james.michalec58@gmial.com>
2020-10-29 14:52:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('div.target');
+}
+
+function increaseRankBy(n){
+  
+  const lis = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li');
+  
+  for(let i=0; i<lis.length; i++){
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML, 10)+n)
+  }
+}
+
+function deepestChild(){
+  const divs = document
+    .getElementById('app')
+    .querySelectorAll('div.grand-node div');
+    
+  console.log('DIVS VARIABLE: '+divs[divs.length].innerHTML)
+  
+ // return divs[divs.length-1].innerHTML.toString();
+}
+
+
+
 
